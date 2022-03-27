e486ec038ca1abffecc1d350cc1317b991eb45af
Leandro Afonso <leandro.afonso@hotmail.com>
2022-03-27 18:40:15 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName =customerName.toUpperCase()
+    return customerName  
+}
+
+console.log(customerName)
+
+function setBestCustomer() {
+    bestCustomer = `not bob`
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = `maybe bob`
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = 'Leo'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = leastFavoriteCustomer.toUpperCase()
+    return leastFavoriteCustomer
+}
